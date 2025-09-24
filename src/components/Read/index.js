ac6e17d3362c0604f0cc76a412525ecf80319c4c
Bollaboina Vamshi Yadav <130643624+vamshiyadavbollaboina@users.noreply.github.com>
2025-09-24 13:02:50 +0530

    Update index.js

--- src/components/Read/index.js
+++ src/components/Read/index.js
@@ -11,9 +11,12 @@ import {
 } from './styledComponents'
 
 const Read = () => {
+  // State to hold user data fetched from API
   const [data, setData] = useState({})
+  // Get user ID from the route params (/read/:id)
   const {id} = useParams()
 
+  // Fetch user details whenever the ID changes
   useEffect(() => {
     axios
       .get(`https://67977c52c2c861de0c6ce6a8.mockapi.io/users/users/${id}`)
@@ -24,7 +27,10 @@ const Read = () => {
   return (
     <Container>
       <Card>
+        {/* Page Title */}
         <Title>Details of User</Title>
+
+        {/* Display user details */}
         <Detail>
           <span>Id:</span> {data.id}
         </Detail>
@@ -40,10 +46,15 @@ const Read = () => {
         <Detail>
           <span>Department:</span> {data.Department}
         </Detail>
+
+        {/* Navigation buttons: Edit & Back */}
         <ButtonGroup>
+          {/* Edit button → navigate to update form */}
           <Button to={`/update/${id}`} variant="edit">
             Edit
           </Button>
+
+          {/* Back button → go to home page */}
           <Button to="/" variant="back">
             Back
           </Button>
